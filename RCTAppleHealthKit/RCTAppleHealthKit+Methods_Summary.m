e13659d2d5823eca3756cc260abf9ef0c7817699
Kyle <kyletherosenberg@gmail.com>
2021-07-09 09:45:28 -0700

    test

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Summary.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Summary.m
@@ -25,7 +25,7 @@
     completion = ^(NSArray<HKActivitySummary *> *results, NSError *error) {
         if (results){
 
-            callback(@[[NSNull null], [results valueForKey:@"activeEnergyBurnedGoal"]]);
+            callback(@[[NSNull null], [results]);
 
             return;
         } else {
